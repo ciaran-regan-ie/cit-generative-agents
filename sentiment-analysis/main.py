59a3987fcac34c5c8be792b40cfe248899aae942
ciaran-regan-ie <ciaranregan123@gmail.com>
2024-04-30 15:40:06 +0900

    add gpt-4

--- sentiment-analysis/main.py
+++ sentiment-analysis/main.py
@@ -14,7 +14,7 @@ client = openai.OpenAI()
 
 def get_sentiment(comment: str) -> dict:
     response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
+        model="gpt-4-turbo",
         messages=[
             {"role": "system", "content": "Determine the overall sentiment of the text as either positive, negative, or neutral, and rate the intensity of this sentiment on a scale from 0 (extremely negative) to 10 (extremely positive). Then, identify which basic emotion—anger, fear, enjoyment, sadness, disgust, or surprise—is most prominently reflected in the text. Based on the words and phrases used, along with the emotional tone, explain why you chose this sentiment rating and emotion. The response should be in JSON format with values for sentiment, intensity, emotion, and explanation."},
             {"role": "user", "content": comment}
@@ -38,16 +38,12 @@ def process_comments(file: str):
     # Remove unnecessary columns
     df = df.drop(columns=["Extra"], errors="ignore")
 
-    df = df.head(3)
-
     # Apply get_sentiment and expand the returned dictionaries into separate columns
     sentiment_data = df['Content'].apply(get_sentiment)
     sentiment_df = pd.DataFrame(sentiment_data.tolist())
 
     # Concatenate the new columns to the original DataFrame
     df = pd.concat([df, sentiment_df], axis=1)
-
-
 
     # Save the updated DataFrame to a new CSV file
     df.to_csv('sentiment-analysis/data/comments_with_sentiment.csv', index=False, sep=';', encoding='ISO-8859-1')
